274c52d686b64112c680cef27dba03569045f3b8
Azat <aelchikushuulu@gmail.com>
2023-02-14 20:43:03 +0600

    Task3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         task1 ();
         task2 ();
+        task3 ();
 
     }
 
@@ -33,6 +34,30 @@ public class Main {
         var paper = 763789;
         System.out.println(paper);
         paper = paper + 4;
+        System.out.println(paper);
+    }
+
+    public static void task3 () {
+
+        var dog = 8.0;
+        System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        dog = dog - 3.5;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        cat = cat - 1.6;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
+        System.out.println(paper);
+        paper = paper - 7639;
         System.out.println(paper);
     }
 
